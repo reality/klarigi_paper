2398918f6180c93645adfe11e790320e388f0317
Luke Slater <tinmachin3@gmail.com>
2022-08-02 22:59:09 +0100

    add enrichment associations

--- mimic_eval/scripts/convert_output.groovy
+++ mimic_eval/scripts/convert_output.groovy
@@ -4,5 +4,9 @@ def tables = new File('klarigi_outputs/overall/main_ecm_withp.out').text.split('
 new File('data/associations/univariate_pn_klarigi.tsv').text = (tables[1] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpneumonia" }.join('\n')
 new File('data/associations/univariate_pe_klarigi.tsv').text = (tables[3] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpulmonary embolism" }.join('\n')
 
-new File('data/associations/multivariate_pn_klarigi.tsv').text = (tables[5] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpneumonia" }.join('\n')
-new File('data/associations/multivariate_pe_klarigi.tsv').text = (tables[7] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpulmonary embolism" }.join('\n')
+def mv_pn = (tables[5] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpneumonia" }.join('\n')
+def mv_pe = (tables[7] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpulmonary embolism" }.join('\n')
+new File('data/associations/multivariate_pn_klarigi.tsv').text = mv_pn
+new File('data/associations/multivariate_pe_klarigi.tsv').text = mv_pe
+new File('data/associations/multivariate_klarigi.tsv').text = mv_pe + '\n' + mv_pn
+
