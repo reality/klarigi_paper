2dcfc8a20dcd69088d4b95c0449dd498c59fbfef
Luke Slater <tinmachin3@gmail.com>
2022-08-03 01:14:53 +0100

    add sim test

--- mimic_eval/scripts/convert_output.groovy
+++ mimic_eval/scripts/convert_output.groovy
@@ -1,8 +1,11 @@
 // this is so stupid
 def tables = new File('klarigi_outputs/overall/main_ecm_withp.out').text.split('tabular')
 
-new File('data/associations/univariate_pn_klarigi.tsv').text = (tables[1] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpneumonia" }.join('\n')
-new File('data/associations/univariate_pe_klarigi.tsv').text = (tables[3] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpulmonary embolism" }.join('\n')
+def uv_pe = (tables[1] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpulmonary embolism" }.join('\n')
+def uv_pn = (tables[3] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpneumonia" }.join('\n')
+new File('data/associations/univariate_pn_klarigi.tsv').text = uv_pn
+new File('data/associations/univariate_pe_klarigi.tsv').text = uv_pe
+new File('data/associations/univariate_klarigi.tsv').text = uv_pe + '\n' + uv_pn
 
 def mv_pn = (tables[5] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpneumonia" }.join('\n')
 def mv_pe = (tables[7] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpulmonary embolism" }.join('\n')
--- mimic_eval/scripts/run_klarigi_train.sh
+++ mimic_eval/scripts/run_klarigi_train.sh
@@ -1,1 +1,1 @@
-time klarigi --data data/train.tsv -o ../hp.owl --verbose --threads 6 --output-type=latex --exclude-classes="HP:0100598;HP:0002090" --output-scores -ecm --reclassify --resnik-ic --min-inclusion=0.1 > klarigi_outputs/overall/main_ecm_withp.out
+time klarigi --data data/train.tsv -o ../hp.owl --verbose --threads 6 --output-type=latex --exclude-classes="HP:0100598;HP:0002090" --output-scores -ecm --reclassify --min-power=0.125 --resnik-ic > klarigi_outputs/overall/main_ecm_withp.out
