de4eb99aef83703a537684d7b73f866e0d0fa15c
Luke Slater <tinmachin3@gmail.com>
2022-08-04 20:20:57 +0100

    okay then, I think that is everything. apart from the READMEs

--- omim_eval/scripts/run_klarigi.sh
+++ omim_eval/scripts/run_klarigi.sh
@@ -1,2 +1,4 @@
 klarigi --data data/phenopackets -pp -o data/hp.owl --verbose --threads 6 --output-scores --group OMIM:616900 --output-type=latex --resnik-ic --min-power=0.2 --reclassify -ecm --perms=2000 > klarigi_outputs/ecm_withp.out 
-#klarigi --data data/phenopackets -pp -o data/hp.owl --verbose --threads 6 --output-scores --group OMIM:616900 --output-type=latex --resnik-ic --min-power=0.2 --reclassify -ecm > klarigi_outputs/ecm_nop.out 
+klarigi --data data/phenopackets -pp -o data/hp.owl --verbose --threads 6 --group OMIM:616900 --output-type=latex --resnik-ic --min-power=0.2 --reclassify -ecm > klarigi_outputs/ecm_nop.out 
+rm data/scores/*
+mv HP_*.txt data/scores
