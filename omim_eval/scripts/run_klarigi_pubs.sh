b2ba6575ca1a0b47ba99e683af02fabd72bd4b5d
Luke Slater <tinmachin3@gmail.com>
2022-08-09 22:38:36 +0100

    rewrite omim eval for new param names. rerun to check no substantive changes.

--- omim_eval/scripts/run_klarigi_pubs.sh
+++ omim_eval/scripts/run_klarigi_pubs.sh
@@ -1,1 +1,1 @@
-klarigi --data data/pp_by_pub.tsv -o data/hp.owl --verbose --threads 6 --output-scores --output-type=latex --resnik-ic --reclassify -ecm > klarigi_outputs/by_pub.out
+klarigi --data data/pp_by_pub.tsv -o data/hp.owl --verbose --threads 6 --output-scores --output-type=latex --resnik-ic --reclassify > klarigi_outputs/by_pub.out
