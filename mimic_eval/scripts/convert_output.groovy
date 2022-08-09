41383fec8ec218d45825d69be19e7e3a9e071095
Luke Slater <tinmachin3@gmail.com>
2022-08-10 00:06:42 +0100

    rewrite mimic eval for new params; check results the same; perm distributions different but otherwise fine. yet to do egl script

--- mimic_eval/scripts/convert_output.groovy
+++ mimic_eval/scripts/convert_output.groovy
@@ -7,8 +7,8 @@ new File('data/associations/univariate_pn_klarigi.tsv').text = uv_pn
 new File('data/associations/univariate_pe_klarigi.tsv').text = uv_pe
 new File('data/associations/univariate_klarigi.tsv').text = uv_pe + '\n' + uv_pn
 
-def mv_pn = (tables[5] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpneumonia" }.join('\n')
-def mv_pe = (tables[7] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpulmonary embolism" }.join('\n')
+def mv_pe = (tables[5] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpulmonary embolism" }.join('\n')
+def mv_pn = (tables[7] =~ /.*(HP\:.......).*/).collect { it[1] + "\tpneumonia" }.join('\n')
 new File('data/associations/multivariate_pn_klarigi.tsv').text = mv_pn
 new File('data/associations/multivariate_pe_klarigi.tsv').text = mv_pe
 new File('data/associations/multivariate_klarigi.tsv').text = mv_pe + '\n' + mv_pn
