bc0e37b9e4b5330bd1b61f98f2805a5d4f6801d3
Luke Slater <tinmachin3@gmail.com>
2022-08-13 16:32:05 +0100

    check no difference in performance on omim side

--- omim_eval/scripts/run_klarigi.sh
+++ omim_eval/scripts/run_klarigi.sh
@@ -1,5 +1,5 @@
 which klarigi
 klarigi --data data/phenopackets -pp -o data/hp.owl --output-scores --verbose --threads 6 --group OMIM:616900 --output-type=latex --resnik-ic --min-r-score=0.2 --reclassify > klarigi_outputs/ecm_nop.out 
-klarigi --data data/phenopackets -pp -o data/hp.owl --verbose --threads 6 --output-scores --group OMIM:616900 --output-type=latex --resnik-ic --min-r-score=0.2 --reclassify --perms=2000 > klarigi_outputs/ecm_withp.out 
-rm data/scores/*
-mv HP_*.txt data/scores
+#klarigi --data data/phenopackets -pp -o data/hp.owl --verbose --threads 6 --output-scores --group OMIM:616900 --output-type=latex --resnik-ic --min-r-score=0.2 --reclassify --perms=2000 > klarigi_outputs/ecm_withp.out 
+#rm data/scores/*
+#mv HP_*.txt data/scores
