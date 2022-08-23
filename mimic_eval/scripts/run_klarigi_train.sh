73e178a45656c5dea4e3d5d21ae82711ce44ec99
Luke Slater <tinmachin3@gmail.com>
2022-08-23 13:35:16 +0100

    updates

--- mimic_eval/scripts/run_klarigi_train.sh
+++ mimic_eval/scripts/run_klarigi_train.sh
@@ -1,6 +1,6 @@
 which klarigi
-#time klarigi --data data/train.tsv -o data/hp.owl --verbose --threads 6 --output-type=latex --output-scores --reclassify --resnik-ic > klarigi_outputs/main_ecm_nop.out
-time klarigi --data data/train.tsv -o data/hp.owl --verbose --threads 6 --output-type=latex --output-scores --reclassify --resnik-ic --perms=2000 > klarigi_outputs/main_ecm_withp.out
+time klarigi --data data/train.tsv -o data/hp.owl --verbose --threads 6 --output-type=latex --output-scores --reclassify --resnik-ic > klarigi_outputs/main_ecm_nop.out
+#time klarigi --data data/train.tsv -o data/hp.owl --verbose --threads 6 --output-type=latex --output-scores --reclassify --resnik-ic --perms=1000 > klarigi_outputs/main_ecm_withp.out
 rm data/scores_examples/*
 mv HP_*.txt data/scores_examples
 groovy scripts/convert_output.groovy
